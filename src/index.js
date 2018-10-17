a39f8edd81b6237b5cc08f36d8fdb518f931f59d
github username <github email address>
2018-10-17 11:47:53 -0500

    hot module webpack added

--- src/index.js
+++ src/index.js
@@ -10,15 +10,26 @@ import { configureStore } from "./app/store/configureStore";
 
 const store = configureStore();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+const rootEl = document.getElementById("root");
 
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
