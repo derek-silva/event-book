a6613c22b928532e541b198b54f902b313c558dc
github username <github email address>
2018-10-18 19:33:57 -0500

    added event detailed page and connected redux to component

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
+import { Link } from "react-router-dom";
 
 class EventListItem extends Component {
   render() {
-    const { event, handleOpenEvent, handleDeleteEvent } = this.props;
+    const { event, handleDeleteEvent } = this.props;
 
     return (
       <Segment.Group>
@@ -47,8 +48,8 @@ class EventListItem extends Component {
             content="Delete"
           />
           <Button
-            onClick={handleOpenEvent(event)}
-            as="a"
+            as={Link}
+            to={`/event/${event.id}`}
             color="teal"
             floated="right"
             content="View"
