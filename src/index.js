cef22a1c92ccac6f614f167b2c599d688a9cf53d
github username <github email address>
2018-10-25 10:40:49 -0500

    added static recent activity to event dashboard

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "semantic-ui-css/semantic.min.css";
+import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
+import ReduxToastr from "react-redux-toastr";
 import "./index.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
@@ -20,6 +22,11 @@ let render = () => {
     <Provider store={store}>
       <BrowserRouter>
         <ScrollToTop>
+          <ReduxToastr
+            position="bottom-right"
+            transitionIn="fadeIn"
+            transitionOut="fadeOut"
+          />
           <App />
         </ScrollToTop>
       </BrowserRouter>
